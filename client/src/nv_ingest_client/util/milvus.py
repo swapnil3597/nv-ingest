236d196598c50f21c94aaaaef5379ae87bb59636
Swapnil Masurekar <swapnil3597@gmail.com>
2025-01-28 10:16:14 +0530

    Update milvus.py | build_algo IVF_PQ

--- client/src/nv_ingest_client/util/milvus.py
+++ client/src/nv_ingest_client/util/milvus.py
@@ -180,7 +180,7 @@ def create_nvingest_index_params(
                 params={
                     "intermediate_graph_degree": 128,
                     "graph_degree": 64,
-                    "build_algo": "NN_DESCENT",
+                    "build_algo": "IVF_PQ",
                     "adapt_for_cpu": "false" if gpu_search else "true",
                 },
             )
